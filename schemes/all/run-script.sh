fcfc8fd55c5a4382deb78ccd30074c1c133f9d4c
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-05-31 13:48:31 -0700

    implement: schemes/all/run-script.sh

---
+++ schemes/all/run-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+adirBase="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../.. && pwd )"
+source "$adirBase/schemes/versions.sh"
+
+for impl in ${impls_of_scheme[@]}
+do
+    echo $impl scheme evaluates:
+    bash "$adirBase/schemes/$impl/run-script.sh" "$@"
+    echo
+done
